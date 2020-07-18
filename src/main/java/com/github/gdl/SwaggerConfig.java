b90c137b7531d810f116885d02832d10d63dfef7
thawat thammawong <thawat.thammawong@gmail.com>
2020-07-18 11:45:10 +0700

    Add root redirect

--- src/main/java/com/github/gdl/SwaggerConfig.java
+++ src/main/java/com/github/gdl/SwaggerConfig.java
@@ -3,11 +3,12 @@ package com.github.gdl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +19,7 @@ public class SwaggerConfig {
 
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(PathSelectors.any())
+                .paths(regex("/v.*"))
                 .build()
                 .apiInfo(apiEndPointsInfo())
                 .enable(true);
